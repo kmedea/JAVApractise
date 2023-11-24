bba90e69ccf3b0a00142b994d9f2f057a03d6e97
kmedea <kerklecz.medea@gmail.com>
2023-11-24 14:12:12 +0100

    basicgame refactor HW 1124

--- AKgyakorl/src/basicgame/BasicGame.java
+++ AKgyakorl/src/basicgame/BasicGame.java
@@ -1,60 +1,58 @@
 package basicgame;
 
+import java.sql.SQLOutput;
 import java.util.Arrays;
 import java.util.Objects;
+import java.util.Random;
 import java.util.SimpleTimeZone;
 
 public class BasicGame {
+
+    static int width = 10; //osztályváltozók
+    static int height = 10;
+    static Random random = new Random();
     public static void main(String[] args) throws InterruptedException {
-        String[][] level = new String[10][10];
+
+        String[][] level = new String[height][width];
+
         String playerMark = "O";
-        int row = 1;
-        int column = 1;
-        Direction direction = Direction.RIGHT;
+        int playerRow = 2;
+        int playerColumn = 2;
+        Direction playerDirection = Direction.RIGHT;
+
+        String enemyMark = "S";
+        int enemyRow = 3;
+        int enemyColumn = 4;
+        Direction enemyDirection = Direction.LEFT;
 
         initLevel(level); //pálya inicializálása - a null helyett ez tölti fel a kétdimenziós tömböt szóközzel és X-el
+        addRandomWall(level, 1,1);
 
         for (int i = 1; i <= 100; i++) {
-            if (i % 10 == 0) {
+            // játékos léptetése
+            if (i % 15 == 0) {
                 //irányváltoztatás
-                direction = changeDirection(direction);
+                playerDirection = changeDirection(playerDirection);
             }
             // ez a lépés
-            row = makeMove(direction, level, row, column)[0];
-            column = makeMove(direction, level, row, column)[1];
-            // kirajzolás
-            draw(level, playerMark, row, column);
+            playerRow = makeMove(playerDirection, level, playerRow, playerColumn)[0];
+            playerColumn = makeMove(playerDirection, level, playerRow, playerColumn)[1];
 
-            System.out.println("----------");
-            Thread.sleep(500L);
-        }
-    }
-    // feladat: 1. pályának legyen egy kerete pl: X
-    // 2. bármilyen irányba megy a karakter, ha egy falba ütközik, akkor ne lépjen arra tovább, hanem maradjon egyhelyben
-    // 3. minden tizedik lépés után változtasson irányt a karakter - jobbra után legyen lefele, utána balra, utána fölfele, majd jobbra,
-    //. játék 100 szor fusson le
-
-    static void draw(String[][] board, String mark, int x, int y) {
-        for (int k = 0; k < board.length; k++) {
-            for (int j = 0; j < board[k].length; j++) {
-                if (k == x && j == y && !board[k][j].equals("X")) {
-                    System.out.print(mark);
-                } else {
-                    System.out.print(board[k][j]);
-                }
+            //ellenfél léptetése
+            if (i % 10 == 0) {
+                //irányváltoztatás
+                enemyDirection= changeDirection(enemyDirection);
             }
-            System.out.println();
+            enemyRow = makeMove(enemyDirection, level, enemyRow, enemyColumn)[0];
+            enemyColumn = makeMove(enemyDirection, level, enemyRow, enemyColumn)[1];
+            // kirajzolás
+            draw(level, playerMark, playerRow, playerColumn, enemyMark, enemyRow, enemyColumn);
+            addSomeDelay(500L);
+            if(playerRow == enemyRow && playerColumn == enemyColumn){
+                break;
+            }
         }
-    }
-
-    static Direction changeDirection(Direction direction) {
-        switch (direction) {
-            case RIGHT -> direction = Direction.DOWN;
-            case DOWN -> direction = Direction.LEFT;
-            case LEFT -> direction = Direction.UP;
-            case UP -> direction = Direction.RIGHT;
-        }
-        return direction;
+        System.out.println("Játék vége!");
     }
 
     static void initLevel(String[][] level) {
@@ -69,39 +67,94 @@ public class BasicGame {
         }
     }
 
-    static Integer[] makeMove(Direction direction, String[][] level, int row, int column) {
-        Integer[] rowAndColumn = new Integer[2];
-        int newRow = row;
-        int newColumn = column;
+    static void addRandomWall (String[][] level, int numberOfHorizontalWalls, int numberOfVerticalWalls){
+        for(int i = 0; i< numberOfHorizontalWalls; i++){
+            addHorizontalWall(level);
+        }
+        for(int i = 0; i< numberOfVerticalWalls; i++){
+            addVerticalWall(level);
+        }
+    }
+    static void addHorizontalWall (String[][] level){
+        int wallWidth = random.nextInt(width-3); // ha adung határt, akkor a 0 és az adott számnál eggyel kisebb számig sorsol
+        int wallRow = random.nextInt(height - 2) + 1;
+        int wallColumn = random.nextInt(width-2 - wallWidth);
+        for(int i = 0; i < wallWidth; i++) {
+            level[wallRow][wallColumn+i] = "X";
+        }
+    }
+    static void addVerticalWall (String[][] level){
+        int wallHeight = random.nextInt(height-3); // ha adung határt, akkor a 0 és az adott számnál eggyel kisebb számig sorsol
+        int wallRow = random.nextInt(height-2-wallHeight);
+        int wallColumn = random.nextInt(width-2)+1;
+        for(int i = 0; i < wallHeight; i++) {
+            level[wallRow+i][wallColumn] = "X";
+        }
+    }
+
+    static Integer[] makeMove(Direction direction, String[][] level, int row, int column) { // pass by value - érték átadás történik a paraméter változóknak (javaban mindig csak érték átadás történik a paraméter változóknál)
         switch (direction) {
             case RIGHT -> {
                 if (level[row][column + 1].equals(" ")) {
-                    newColumn++;
+                    column++;
                 }
             }
             case LEFT -> {
                 if (level[row][column - 1].equals(" ")) {
-                    newColumn--;
+                    column--;
                 }
             }
             case UP -> {
                 if (level[row - 1][column].equals(" ")) {
-                    newRow--;
+                    row--;
                 }
             }
             case DOWN -> {
                 if (level[row + 1][column].equals(" ")) {
-                    newRow++;
+                    row++;
                 }
             }
         }
-        rowAndColumn[0] = newRow;
-        rowAndColumn[1] = newColumn;
-        return rowAndColumn;
+        return new Integer[] {row, column};
     }
+
+    static Direction changeDirection(Direction direction) {
+        switch (direction) {
+            case RIGHT -> direction = Direction.DOWN;
+            case DOWN -> direction = Direction.LEFT;
+            case LEFT -> direction = Direction.UP;
+            case UP -> direction = Direction.RIGHT;
+        }
+        return direction;
+    }
+
+    static void draw(String[][] board, String mark, int x, int y, String enemyMark, int enemyRaw, int enemyColumn) {
+        for (int k = 0; k < board.length; k++) {
+            for (int j = 0; j < board[k].length; j++) {
+                if (k == x && j == y && !board[k][j].equals("X")) {
+                    System.out.print(mark);
+                }else if (k == enemyRaw && j == enemyColumn && !board[k][j].equals("X")){
+                    System.out.print(enemyMark);
+                } else {
+                    System.out.print(board[k][j]);
+                }
+            }
+            System.out.println();
+        }
+    }
+    static void addSomeDelay(long timeout) throws InterruptedException {
+        System.out.println("----------");
+        Thread.sleep(timeout);
+    }
+    // feladat: 1. pályának legyen egy kerete pl: X
+    // 2. bármilyen irányba megy a karakter, ha egy falba ütközik, akkor ne lépjen arra tovább, hanem maradjon egyhelyben
+    // 3. minden tizedik lépés után változtasson irányt a karakter - jobbra után legyen lefele, utána balra, utána fölfele, majd jobbra,
+    //. játék 100 szor fusson le
 
 
     //Házi: 1. pálya inicializálását kiemelni külön metódusba a név: initLevel();
     // 2. a karakter mozgatását végző logikát is ki kell emelni egy külön metódusba, neve: makeMove(),
-    // bemeneti paraméterként kapja meg a karakter aktuális sor és oszlop adatát, visszatérési értékként adja vissza az új koordinátákat egy int tömben
+    // bemeneti paraméterként kapja meg a karakter aktuális sor és oszlop adatát, visszatérési értékként adja vissza az új koordinátákat egy int tömbben
+    // 3. ha az ellenfél koordinátája megegyezik a játékos koordinátájával, akkor álljon le és írja ki, hogy játék vége
+    // 4. legyen valahol egy függőleges és egy vízszintes fal a pályán belül ennek elhelyezését egy külön metódus végezze
 }
