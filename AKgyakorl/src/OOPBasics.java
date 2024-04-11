92af8197a3e57a220afff5eb4c53b52e785a6733
kmedea <kerklecz.medea@gmail.com>
2024-04-11 15:24:52 +0200

    OOP 20240411

--- AKgyakorl/src/OOPBasics.java
+++ AKgyakorl/src/OOPBasics.java
@@ -3,7 +3,8 @@ public class OOPBasics {
     /* OOP -  Object Oriented Programming
     - egységbezárás - encapsulation: az összetartozó részeket egy helyre írjuk - tagoljuk a program részeket
     - öröklődés - inheritance - az öröklődés lehetővé teszi azt, hogy újra lehessen hasznosítani a már megírt kódunkat
-    -
+    - polimorfizmus - polymorphism - sokalakuság - vannak olyan kódrészletek, amelyek nagyon hasonlóak és különböző szituációkban, különböző típusokat kell használni.
+    - absztrakció - abstraction -
      */
 
 }
