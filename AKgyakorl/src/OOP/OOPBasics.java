aa58a7f58f3d255bd9b6fec20025766701baa648
kmedea <kerklecz.medea@gmail.com>
2024-04-12 14:44:19 +0200

    OOP 20240412

--- AKgyakorl/src/OOP/OOPBasics.java
+++ AKgyakorl/src/OOP/OOPBasics.java
@@ -7,6 +7,39 @@ public class OOPBasics {
     - öröklődés - inheritance - az öröklődés lehetővé teszi azt, hogy újra lehessen hasznosítani a már megírt kódunkat
     - polimorfizmus - polymorphism - sokalakuság - vannak olyan kódrészletek, amelyek nagyon hasonlóak és különböző szituációkban, különböző típusokat kell használni.
     - absztrakció - abstraction - Az absztrakció azt jelenti, hogy azonosítjuk valaminek a fő, legjelentősebb jellemzőit, miközben egyidejűleg elvetünk minden kisebbet és jelentéktelent.
+
+     https://codegym.cc/hu/groups/posts/hu.155.oop-alapelvek
+     */
+
+    /*Pass by value !!!
+
+    érték szerinti átadás - a java csak így működik
+    amikor a program egyik része át akar egy másik résznek adni egy értéket, akkor hogyan tudja ezt megtenni
+    a java mindig értéket fog átadni
+     */
+
+    /*Primitív típusú változónál:
+    int year; deklaráljuk a változót - ennek hatására a memóriában létre jön egy int típusú year nevű változó - az int 32 bites ezért 4 byte-ot foglalódik le a memóriában
+    ott fog ez létrejönni, ahol van szabad hely, arra nincs ráhatásunk, hogy hol hozza létre a memóriában
+
+    year = 2020; értéket adunk neki - a jobboldal értéke belemásolódik a baloldal értékébe - a 2020 bináris változata kerül be a memóriaterületre
+     */
+
+    /*Referencia típusú változóknál:
+    class Date {
+        int year;
+        int month;
+        int day;
+        }
+
+    Date birthday; - deklarálun egy Date típusú változót - ekkor egy helyet lefoglal a memóriában (ua. mint a primitívnél)
+    birthday = new Date(); -
+    birthday.year = 2020;
+    birthday.month = 6;
+    birthday.day = 29;
+
+
+
      */
 
 }
